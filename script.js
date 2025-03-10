e37166e4517cbd82e7679250f23778b35912549e
GeotechRiskConsulting <geotechriskconsulting@gmail.com>
2025-03-10 18:07:40 -0500

    Delete script.js
    del

--- script.js
+++
@@ -1,5 +0,0 @@
-// Funcionalidad del menú hamburguesa
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active'); // Alternar la clase "active"
-});
